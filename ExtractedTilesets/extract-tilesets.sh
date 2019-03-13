c73bec87282af5beac5842c2302d5dc987f74e34
cjdaly <cjdaly@locosoft.net>
2019-03-13 01:13:25 -0700

    tiling n' stylin'

--- ExtractedTilesets/extract-tilesets.sh
+++ ExtractedTilesets/extract-tilesets.sh
@@ -30,7 +30,7 @@ DT2_png=../DungeonTilesetII/0x72_DungeonTilesetII_v1.2.png
 rm -rf tileset1
 mkdir tileset1
 
-convert $DT2_png -crop "16x24+$[tile16*8]+8" tileset1/elf_f.bmp
+convert $DT2_png -crop "16x24+$[tile16*8]+$[tile32*0+8]" tileset1/elf_f.bmp
 convert $DT2_png -crop "16x24+$[tile16*8]+$[tile32*1+8]" tileset1/elf_m.bmp
 convert $DT2_png -crop "16x24+$[tile16*8]+$[tile32*2+8]" tileset1/knight_f.bmp
 convert $DT2_png -crop "16x24+$[tile16*8]+$[tile32*3+8]" tileset1/knight_m.bmp
@@ -49,6 +49,47 @@ convert $DT2_png -crop "16x16+$[tile16*18]+$[tile16*14]" tileset1/potion_red.bmp
 convert $DT2_png -crop "16x16+$[tile16*19]+$[tile16*14]" tileset1/potion_blue.bmp
 convert $DT2_png -crop "16x16+$[tile16*20]+$[tile16*14]" tileset1/potion_green.bmp
 convert $DT2_png -crop "16x16+$[tile16*21]+$[tile16*14]" tileset1/potion_yellow.bmp
+
+convert $DT2_png -crop "16x16+$[tile16*1]+$[tile16*1]" tileset1/wall_left.bmp
+convert $DT2_png -crop "16x16+$[tile16*2]+$[tile16*1]" tileset1/wall_mid.bmp
+convert $DT2_png -crop "16x16+$[tile16*2]+$[tile16*10]" tileset1/wall_mid2.bmp
+convert $DT2_png -crop "16x16+$[tile16*3]+$[tile16*10]" tileset1/wall_mid3.bmp
+convert $DT2_png -crop "16x16+$[tile16*3]+$[tile16*1]" tileset1/wall_right.bmp
+convert $DT2_png -crop "16x16+$[tile16*1]+$[tile16*2]" tileset1/wall_banner_red.bmp
+convert $DT2_png -crop "16x16+$[tile16*2]+$[tile16*2]" tileset1/wall_banner_blue.bmp
+convert $DT2_png -crop "16x16+$[tile16*1]+$[tile16*3]" tileset1/wall_banner_green.bmp
+convert $DT2_png -crop "16x16+$[tile16*2]+$[tile16*3]" tileset1/wall_banner_yellow.bmp
+convert $DT2_png -crop "16x16+$[tile16*3]+$[tile16*2]" tileset1/wall_hole1.bmp
+convert $DT2_png -crop "16x16+$[tile16*3]+$[tile16*3]" tileset1/wall_hole2.bmp
+convert $DT2_png -crop "16x16+$[tile16*4]+$[tile16*5]" tileset1/wall_goo.bmp
+
+convert $DT2_png -crop "16x16+$[tile16*1]+$[tile16*0]" tileset1/walltop_left.bmp
+convert $DT2_png -crop "16x16+$[tile16*2]+$[tile16*0]" tileset1/walltop_mid.bmp
+convert $DT2_png -crop "16x16+$[tile16*3]+$[tile16*0]" tileset1/walltop_right.bmp
+convert $DT2_png -crop "16x16+$[tile16*0]+$[tile16*7]" tileset1/walltop_R4.bmp
+convert $DT2_png -crop "16x16+$[tile16*1]+$[tile16*7]" tileset1/walltop_L4.bmp
+convert $DT2_png -crop "16x16+$[tile16*0]+$[tile16*8]" tileset1/walltop_R1.bmp
+convert $DT2_png -crop "16x16+$[tile16*1]+$[tile16*8]" tileset1/walltop_L1.bmp
+convert $DT2_png -crop "16x16+$[tile16*2]+$[tile16*8]" tileset1/walltop_L2.bmp
+convert $DT2_png -crop "16x16+$[tile16*3]+$[tile16*8]" tileset1/walltop_R2.bmp
+convert $DT2_png -crop "16x16+$[tile16*0]+$[tile16*9]" tileset1/walltop_R3.bmp
+convert $DT2_png -crop "16x16+$[tile16*1]+$[tile16*9]" tileset1/walltop_L3.bmp
+convert $DT2_png -crop "16x16+$[tile16*2]+$[tile16*9]" tileset1/walltop_LL.bmp
+convert $DT2_png -crop "16x16+$[tile16*3]+$[tile16*9]" tileset1/walltop_LR.bmp
+
+convert $DT2_png -crop "16x16+$[tile16*1]+$[tile16*4]" tileset1/floor1.bmp
+convert $DT2_png -crop "16x16+$[tile16*2]+$[tile16*4]" tileset1/floor2.bmp
+convert $DT2_png -crop "16x16+$[tile16*3]+$[tile16*4]" tileset1/floor3.bmp
+convert $DT2_png -crop "16x16+$[tile16*1]+$[tile16*5]" tileset1/floor4.bmp
+convert $DT2_png -crop "16x16+$[tile16*2]+$[tile16*5]" tileset1/floor5.bmp
+convert $DT2_png -crop "16x16+$[tile16*3]+$[tile16*5]" tileset1/floor6.bmp
+convert $DT2_png -crop "16x16+$[tile16*1]+$[tile16*6]" tileset1/floor7.bmp
+convert $DT2_png -crop "16x16+$[tile16*2]+$[tile16*6]" tileset1/floor8.bmp
+convert $DT2_png -crop "16x16+$[tile16*3]+$[tile16*6]" tileset1/floor_ladder.bmp
+convert $DT2_png -crop "16x16+$[tile16*4]+$[tile16*6]" tileset1/floor_goo.bmp
+convert $DT2_png -crop "16x16+$[tile16*6]+$[tile16*8]" tileset1/floor_wall.bmp
+convert $DT2_png -crop "16x16+$[tile16*6]+$[tile16*9]" tileset1/floor_hole.bmp
+convert $DT2_png -crop "16x16+$[tile16*5]+$[tile16*11]" tileset1/floor_stairs.bmp
 
 convert $DT2_png -crop "64x48+$[tile16*1]+$[tile16*13]" tileset1/gateway.bmp
 
