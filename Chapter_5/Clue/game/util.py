a6e15e3e022e0546b882b0abfd38abcab339cd7f
Chris <cjdaly@locosoft.net>
2020-07-26 17:10:33 -0700

    re-org...

--- Chapter_5/Clue/game/util.py
+++ Chapter_5/Clue/game/util.py
@@ -21,37 +21,26 @@
 # THE SOFTWARE.
 
 
-import terminalio, displayio, board
-from adafruit_clue import clue
-from game import util
+import displayio
+from adafruit_display_text import label
 
-def play():
-  game={}
-  DSP=board.DISPLAY ; game['DSP']=DSP
-  game['group']=displayio.Group(max_size=8)
-  #
-  M = util.init_tilegrid(game, "ClueWorld","map", 1,1,500,500, 0,0)
-  #
-  textA = util.init_label(game, "textA", terminalio.FONT, 20, 0xFF80FF, 1, 200, "A")
-  textB = util.init_label(game, "textB", terminalio.FONT, 20, 0xFF80FF, 200, 200, "B")
-  textT = util.init_label(game, "textT", terminalio.FONT, 40, 0xFFFFFF, 1, 1, "Hello!!!")
-  #
-  DSP.show(game['group'])
-  #
-  updateT = False
-  while True:
-    if clue.button_a:
-      M.x -= 10 ; updateT = True
-    elif clue.button_b:
-      M.x += 10 ; updateT = True
-    elif clue.touch_0:
-      M.y -= 10 ; updateT = True
-    elif clue.touch_1:
-      pass
-    elif clue.touch_2:
-      M.y += 10 ; updateT = True
-    #
-    if updateT:
-      textT.text = "x: " + str(M.x) + ", y: " + str(M.y)
-      upateT = False
+def load_tilegrid(filename,w,h,tw,th):
+  f = open("/game/maps/" + filename + ".bmp", "rb")
+  odb = displayio.OnDiskBitmap(f)
+  tg=displayio.TileGrid(odb, pixel_shader=displayio.ColorConverter(), width=w, height=h, tile_width=tw, tile_height=th)
+  return tg
+
+def init_tilegrid(game, filename,name, w,h,tw,th, x,y):
+  tg=load_tilegrid(filename, w,h,tw,th)
+  tg.x=x ; tg.y=y
+  game['group'].append(tg)
+  game[name]=tg
+  return tg
+
+def init_label(game, name, font, len, color, x,y, text):
+  lbl=label.Label(font, max_glyphs=len, color=color)
+  lbl.x=x ; lbl.y=y; lbl.text=text
+  game['group'].append(lbl)
+  game[name]=lbl
+  return lbl
 
